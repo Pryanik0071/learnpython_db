e1a5ded72ae50ec181ab9551fa99b7daef0cde41
Dmitry <di.mesler.2010@yandex.ru>
2021-10-18 14:52:59 +0300

    Add: config.py

--- db.py
+++ db.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('postgresql://postgres:admin@localhost:5432/learnpython_db')
+from config import SQLALCHEMY_DATABASE_URI
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
