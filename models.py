cc97e82767b8432b68a7ff8650ac9786f160b7f9
Dmitry <di.mesler.2010@yandex.ru>
2021-10-19 12:14:49 +0300

    Add: save fake data on db

--- models.py
+++ models.py
@@ -1,9 +1,29 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
+
 from db import Base, engine
+
+
+class Salary(Base):
+    __tablename__ = "salaries"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    city = Column(String)
+    address = Column(String)
+    company = Column(String)
+    job = Column(String)
+    phone_number = Column(String)
+    email = Column(String)
+    date_of_birth = Column(Date)
+    salary = Column(Integer)
+
+    def __repr__(self):
+        return f"Salary {self.id}, {self.name}, {self.company}"
 
 
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
     salary = Column(Integer)
